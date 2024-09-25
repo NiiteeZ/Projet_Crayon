c79ea20fdacf34962eab048187b01e4fa9aa5ad9
NiiteeZ <etsylvain@hotmail.com>
2024-09-25 18:30:32 +0200

    msg

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -4,12 +4,3 @@ from . import models
 
 admin.site.register(models.Ville)
 admin.site.register(models.Local)
-admin.site.register(models.SiegeSocial)
-admin.site.register(models.Machine)
-admin.site.register(models.Usine)
-admin.site.register(models.Ressource)
-admin.site.register(models.QuantiteRessource)
-admin.site.register(models.Objet)
-admin.site.register(models.Stock)
-admin.site.register(models.Etape)
-admin.site.register(models.Produit)
--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Ville(models.Model):
     nom = models.CharField(max_length=100)
     code_postal = models.IntegerField(default=0)
-    prix = models.IntegerField(default=0)
+    prix_par_m2 = models.IntegerField(default=0)
 
     def __str__(self):
         return self.nom
@@ -18,3 +18,24 @@ class Local(models.Model):
 
     def __str__(self):
         return self.nom
+
+
+class SiegeSocial(Local):  # heritage siege herite de local
+    def __str__(self):
+        return f"Siège social à {self.ville.nom}"
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField(default=0)
+    n_serie = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nom
+
+
+class Usine(Local):  # heritage Usine herite de local
+    machines = models.ManyToManyField(Machine)
+
+    def __str__(self):
+        pass
