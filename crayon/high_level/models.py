dd5cca7fed6f452ec9ebcb12b40ac96916afe18f
NiiteeZ <etsylvain@hotmail.com>
2024-09-25 12:00:52 +0200

    new

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -7,66 +7,80 @@ class Ville(models.Model):
     code_postal = models.IntegerField(default=0)
     prix = models.IntegerField(defaut=0)
 
-    def __init__(self, nom, code_postal, prix):
-        self.nom = nom
-        self.code_postal = code_postal
-        self.prix = prix
+    def __str__(self):
+        pass
 
 
 class Local(models.Model):
     nom = models.CharField(max_length=100)
     surface = models.IntegerField(default=0)
-    ville = models.ForeignKey(Ville)
+    ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
 
-    def __init__(self, nom, surface):
-        self.nom = nom
-        self.ville = Ville()  # composition
-        self.surface = surface
+    def __str__(self):
+        pass
 
 
 class SiegeSocial(Local):  # heritage siege herite de local
-    def __init__(self):
+    def __str__(self):
+        pass
+
+
+class Machine(models.Model):
+    nom = models.CharField(max_length=100)
+    prix = models.IntegerField(default=0)
+    n_serie = models.IntegerField(default=0)
+
+    def __str__(self):
         pass
 
 
 class Usine(Local):  # heritage Usine herite de local
-    def __init__(self, machines):
-        self.machines = machines
+    machines = models.ManyToManyField(Machine)
+
+    def __str__(self):
+        pass
 
 
-class Machine:
-    def __init__(self, nom, prix, n_serie):
-        self.nom = nom
-        self.prix = prix
-        self.n_serie = n_serie
-
-
-class Objet:
+class Objet(models.Model):
     def __init__(self, nom, prix):
         self.nom = nom
         self.prix = prix
 
 
 class Ressource(Objet):
-    def __init__(self):
+    def __str__(self):
+        pass
+
+
+class QuantiteRessource(models.Model):
+    quantite_ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
+    quantite = models.IntegerField(default=0)
+
+    def __str__(self):
         pass
 
 
 class Produit(Objet):
-    def __init__(self, premiere_etape):
-        self.premiere_etape = premiere_etape
+    premiere_etape = models.CharField(max_length=100)
+
+    def __str__(self):
+        pass
 
 
 class Stock:
-    def __init__(self, nombre):
-        self.objet = Objet()
-        self.nombre = nombre
+    objet = models.ForeignKey(Objet, on_delete=models.PROTECT)
+    nombre = models.IntegerField(default=0)
+
+    def __str__(self):
+        pass
 
 
 class Etape:
-    def __init__(self, nom, machine, quantite_ressource, duree):
-        self.nom = nom
-        self.machine = machine
-        self.noquantite_ressourcem = quantite_ressource
-        self.duree = duree
-        self.etape_suivante = None
+    nom = models.CharField(max_length=100)
+    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    quantite_ressource = models.ForeignKey(QuantiteRessource)
+    duree = models.IntegerField(default=0)
+    etape_suivant = models.ForeignKey("self", on_delete=models.PROTECT)
+
+    def __str__(self):
+        pass
