74a4b7bc16b63ae60a0fd045a7d53db250138cae
NiiteeZ <etsylvain@hotmail.com>
2024-09-25 18:16:04 +0200

    retest

--- crayon/high_level/models.py
+++ crayon/high_level/models.py
@@ -5,82 +5,16 @@ from django.db import models
 class Ville(models.Model):
     nom = models.CharField(max_length=100)
     code_postal = models.IntegerField(default=0)
-    prix = models.IntegerField(defaut=0)
+    prix = models.IntegerField(default=0)
 
     def __str__(self):
-        pass
+        return self.nom
 
 
 class Local(models.Model):
     nom = models.CharField(max_length=100)
+    ville = models.ForeignKey(Ville, on_delete=models.CASCADE)
     surface = models.IntegerField(default=0)
-    ville = models.ForeignKey(Ville, on_delete=models.PROTECT)
 
     def __str__(self):
-        pass
-
-
-class SiegeSocial(Local):  # heritage siege herite de local
-    def __str__(self):
-        pass
-
-
-class Machine(models.Model):
-    nom = models.CharField(max_length=100)
-    prix = models.IntegerField(default=0)
-    n_serie = models.IntegerField(default=0)
-
-    def __str__(self):
-        pass
-
-
-class Usine(Local):  # heritage Usine herite de local
-    machines = models.ManyToManyField(Machine)
-
-    def __str__(self):
-        pass
-
-
-class Objet(models.Model):
-    def __init__(self, nom, prix):
-        self.nom = nom
-        self.prix = prix
-
-
-class Ressource(Objet):
-    def __str__(self):
-        pass
-
-
-class QuantiteRessource(models.Model):
-    quantite_ressource = models.ForeignKey(Ressource, on_delete=models.PROTECT)
-    quantite = models.IntegerField(default=0)
-
-    def __str__(self):
-        pass
-
-
-class Produit(Objet):
-    premiere_etape = models.CharField(max_length=100)
-
-    def __str__(self):
-        pass
-
-
-class Stock:
-    objet = models.ForeignKey(Objet, on_delete=models.PROTECT)
-    nombre = models.IntegerField(default=0)
-
-    def __str__(self):
-        pass
-
-
-class Etape:
-    nom = models.CharField(max_length=100)
-    machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
-    quantite_ressource = models.ForeignKey(QuantiteRessource)
-    duree = models.IntegerField(default=0)
-    etape_suivant = models.ForeignKey("self", on_delete=models.PROTECT)
-
-    def __str__(self):
-        pass
+        return self.nom
