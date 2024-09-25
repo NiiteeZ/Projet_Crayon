b7c7bfae558a876822da936e220a928466b8580a
NiiteeZ <etsylvain@hotmail.com>
2024-09-25 18:49:28 +0200

    lst

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -5,8 +5,3 @@ from . import models
 admin.site.register(models.Ville)
 admin.site.register(models.Local)
 admin.site.register(models.SiegeSocial)
-admin.site.register(models.Machine)
-admin.site.register(models.Usine)
-admin.site.register(models.Ressource)
-admin.site.register(models.QuantiteRessource)
-admin.site.register(models.Objet)
