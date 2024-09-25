ee66ecbecd6369bde73c3ace53ec6ec281de91f8
NiiteeZ <etsylvain@hotmail.com>
2024-09-25 19:02:55 +0200

    qnt

--- crayon/high_level/admin.py
+++ crayon/high_level/admin.py
@@ -5,3 +5,7 @@ from . import models
 admin.site.register(models.Ville)
 admin.site.register(models.Local)
 admin.site.register(models.SiegeSocial)
+admin.site.register(models.Machine)
+admin.site.register(models.Usine)
+admin.site.register(models.Ressource)
+admin.site.register(models.QuantiteRessource)
